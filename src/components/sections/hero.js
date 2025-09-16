82f59fdff9f80eb6711d218249d6a2990b046daf
Arinzelight <arinzelight2@gmail.com>
2025-09-16 09:58:12 +0100

    modify my hero section info

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -67,9 +67,9 @@ const Hero = () => {
       <p>
         I am a software engineer with a passion for creating exceptional digital experiences,
         blending development and design when needed. Currently, I focus on building accessible,
-        human-centered products
+        human-centered products at {''}
         <a href="https://edubridge.com.ng/" target="_blank" rel="noreferrer">
-          @Edubridge
+          Edubridge
         </a>
         .
       </p>
