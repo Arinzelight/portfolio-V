3d3b0c8ae4f61084aaf71293621718c19da78bf5
Ezinwa Arinze <arinzelight2@gmail.com>
2026-09-23 21:04:13 +0100

    chore: update linkedIn url

--- src/config.js
+++ src/config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     {
       name: 'Linkedin',
-      url: 'www.linkedin.com/in/matrixcode',
+      url: 'https://www.linkedin.com/in/matrixcode/',
     },
     // {
     //   name: 'Codepen',
